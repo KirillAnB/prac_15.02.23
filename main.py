c49f913557a06f6fa0326c77e3a873347068a32f
KirillAnB <it-reso@mail.ru>
2023-02-19 15:24:52 +0300

    Функция для валидации скобок в строке

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ class Queue:
         Очередь с помощью python list
         Начало очередм - жлемент списка с индексом 0, конец очереди - элемент с индексом len(my_queue)-1
         """
-        self.my_queue = []  # TODO инициализировать список
+        self.my_queue = []
 
     def enqueue(self, elem: Any) -> None:
         """
-        Добавление элемент в конец очереди
+        Добавление элемент в конец очереди.
+        Сложность метода О(1).
 
         :param elem: Элемент, который должен быть добавлен
         """
@@ -22,13 +23,13 @@ class Queue:
 
     def dequeue(self) -> Any:
         """
-        Извлечение элемента из начала очереди.
+        Извлечение элемента из начала очереди. Сложность О(1).
 
         :raise: IndexError - Ошибка, если очередь пуста
 
         :return: Извлеченный с начала очереди элемент.
         """
-        if not len(self.my_queue) > 0:
+        if not self.my_queue:
             raise IndexError('Очередь пуста')
         elem_to_remove = self.my_queue.pop(0)
         return elem_to_remove
@@ -36,6 +37,7 @@ class Queue:
     def peek(self, ind: int = 0) -> Any:
         """
         Просмотр произвольного элемента, находящегося в очереди, без его извлечения.
+        Сложность доступа к элементу равна О(1).
 
         :param ind: индекс элемента (отсчет с начала, 0 - первый с начала элемент в очереди, 1 - второй с начала элемент в очереди, и т.д.)
 
@@ -46,17 +48,18 @@ class Queue:
         """
         if not isinstance(ind, int):
             raise TypeError('Индекс элемента не типа integer!')
-        elem_to_check = self.my_queue[ind - 1]  # TODO реализовать метод peek
-        if ind < 0 or ind > len(self.my_queue) :
+        if ind < 0 or ind > len(self.my_queue):
             raise IndexError('Индекс вне границ очереди!')
+        elem_to_check = self.my_queue[ind - 1]
         return elem_to_check
 
     def clear(self) -> None:
-        """ Очистка очереди. """
-        self.my_queue.clear()  # TODO реализовать метод clear
+        """ Очистка очереди. Сложность О(1). """
+
+        self.my_queue.clear()  #
 
     def __len__(self):
-        """ Количество элементов в очереди. """
+        """ Количество элементов в очереди. Сложность равна О(n)"""
         sum_ = 0
         for _ in self.my_queue:
             sum_ += 1
